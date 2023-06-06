392127885e4c9b9920d7f15fb9ac07c4207f0393
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-06-06 10:10:57 -0600

    tabla recetas

--- vista/menu/consultorio/contenido/js/guardar-consultorio.js
+++ vista/menu/consultorio/contenido/js/guardar-consultorio.js
@@ -1,4 +1,4 @@
- function alertaConsultorio(btn){
+function alertaConsultorio(btn) {
 
     alertMensajeConfirm({
         title: '¿Está seguro que relleno bien el o los campos?',
@@ -9,49 +9,51 @@
         cancelButtonColor: '#d33',
         confirmButtonText: 'Si',
         cancelButtonText: 'No'
-    }, function(){
-       guardarDatosConsultorio(btn)
-    } ,1)
- }
+    }, function () {
+        guardarDatosConsultorio(btn)
+    }, 1)
+}
 
-$(document).on('click', '#btn-guardar-nota-consulta', function(event){
+
+$(document).on('click', '#btn-guardar-nota-consulta', function (event) {
     event.preventDefault()
     alertaConsultorio('nota_consulta')
 })
-$(document).on('click', '#btn-agregar-exploracion-clinina', function(event){
+$(document).on('click', '#btn-agregar-exploracion-clinina', function (event) {
     event.preventDefault()
     alertaConsultorio('exploracion_fisica')
 })
-$(document).on('click', '#btn-guardar-Diagnostico', function(event){
+$(document).on('click', '#btn-guardar-Diagnostico', function (event) {
     event.preventDefault()
     alertaConsultorio('diagostico')
 })
 //aqui va solicitud de estudios
 
 
-$(document).on('click', '#btn-guardar-Receta', function(event){
+$(document).on('click', '#btn-guardar-Receta', function (event) {
     event.preventDefault()
     alertaConsultorio('receta')
 })
-$(document).on('click', '#btn-guardar-plan-tratamiento', function(event){
+$(document).on('click', '#btn-guardar-plan-tratamiento', function (event) {
     event.preventDefault()
     alertaConsultorio('plan_tratamiento')
 })
 
 
 //Insertar datos en consultorio
-function guardarDatosConsultorio(btn){
+function guardarDatosConsultorio(btn) {
 
-    switch(btn){
+    switch (btn) {
 
         case 'nota_consulta':
-            let dataJson_nota ={
+            let dataJson_nota = {
                 api: 1,
                 turno_id: pacienteActivo.array['ID_TURNO'],
                 notas_consulta: $('#nota-consulta-campo-consulta').val(),
             }
-            ajaxAwait(dataJson_nota, 'consultorio2_api', {callbackAfter: true}, false, function(data){
+            ajaxAwait(dataJson_nota, 'consultorio2_api', { callbackAfter: true }, false, function (data) {
                 alertMensaje('success', 'Datos guardados', 'Espere un momento...', null, null, 1500)
+
             })
             break;
 
@@ -83,21 +85,21 @@ function guardarDatosConsultorio(btn){
             let dataJson_recetas = {
                 api: 1,
                 turno_id: pacienteActivo.array['ID_TURNO'],
-                nombre_generico : $("#nombre_generico").val(),
-                nombre_comercial : $("#nombre_comercial").val(),
-                forma_farmaceutica : $("#forma_farmaceuticaval").val(),
-                dosis : $("#dosis").val(),
-                presentacion : $("#presentacion").val(),
-                frecuencia : $("#frecuencia").val(),
-                via_de_administracion : $("#via_de_administracion").val(),
-                duracion_de_tratamiento : $("#duracion_de_tratamiento").val(),
-                indicaciones_de_uso : $("#indicaciones_de_uso").val()
+                nombre_generico: $("#nombre_generico").val(),
+                nombre_comercial: $("#nombre_comercial").val(),
+                forma_farmaceutica: $("#forma_farmaceuticaval").val(),
+                dosis: $("#dosis").val(),
+                presentacion: $("#presentacion").val(),
+                frecuencia: $("#frecuencia").val(),
+                via_de_administracion: $("#via_de_administracion").val(),
+                duracion_de_tratamiento: $("#duracion_de_tratamiento").val(),
+                indicaciones_de_uso: $("#indicaciones_de_uso").val()
             }
             ajaxAwait(dataJson_recetas, 'consultorio_recetas_api', { callbackAfter: true }, false, function (data) {
                 alertMensaje('success', 'Datos guardados', 'Espere un momento...', null, null, 1500)
             })
             break;
-        
+
         case 'plan_tratamiento':
             dataJson_tratatiento = {
                 api: 1,
@@ -107,16 +109,54 @@ function guardarDatosConsultorio(btn){
             ajaxAwait(dataJson_tratatiento, 'consultorio2_api', { callbackAfter: true }, false, function (data) {
                 alertMensaje('success', 'Datos guardados', 'Espere un momento...', null, null, 1500)
             })
-            break;    
-            
+            break;
+
         default:
             alertToast()
-            break;    
+            break;
     }
 }
 
 
-function mostrarTablaRecetas(){
 
-}
+//Tabla de recetas
+tablaListaRecetas = $("#tablaListaRecetas").DataTable({
+    language: { url: "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json", },
+    lengthChange: false,
+    info: false,
+    paging: false,
+    scrollY: autoHeightDiv(0, 284),
+    scrollCollapse: true,
+    ajax: {
+        dataType: 'json',
+        data: { api: 2, id_turno: pacienteActivo.array['ID_TURNO'] },
+        method: 'POST',
+        url: `${http}${servidor}/${appname}/api/consultorio_recetas_api.php`,
+        beforeSend: function () {
+        },
+        complete: function () {
+            tablaListaRecetas.columns.adjust().draw()
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alertErrorAJAX(jqXHR, textStatus, errorThrown);
+        },
+        dataSrc: 'response.data'
+    },
+    columns: [
+        { data: 'COUNT' },
+        {
+            data: 'FECHA_REGISTRO', render: function (data) {
+                return formatoFecha2(data, [0, 1, 3, 0]).toUpperCase();
+            }
+        },
+        { data: 'FOLIO' }
+    ],
+    columnDefs: [
+        { target: 0, title: '#', className: 'all' },
+        { target: 1, title: 'Nombre Comercial', className: 'all' },
+        { target: 2, title: 'Folio', className: 'none' }
 
+    ]
+})
+
+//tablalistaRecetas.ajax.reload()
