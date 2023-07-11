13e397fa92e00aef167f18fc643315d1e872a551
Cuevas113 <96274965+Cuevas113@users.noreply.github.com>
2023-07-11 16:08:35 -0600

    Traducción

--- vista/registro/modals/registro.php
+++ vista/registro/modals/registro.php
@@ -2,7 +2,9 @@
 // include "registrar-paciente.php";
 // include "registrar-pruebas.php";
 // include "consultar-resultado.php";
-include '../../include/modal/registrar-pruebas.php';
-include '../../include/modal/registrar-paciente.php';
+$language = isset($_POST['language']) ? $_POST['language'] : '';
 
-?>
+// echo "../../include/modal/registrar-pruebas$language.php";
+
+include "../../include/modal/registrar-pruebas$language.php";
+include "../../include/modal/registrar-paciente$language.php";
