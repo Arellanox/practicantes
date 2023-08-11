76d389af8408f230df48a2b8f6c2baa230e4375a
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-08-11 12:56:45 -0600

    modal referencia

--- vista/menu/laboratorio-estudios/modals/modal_referencia.php
+++ vista/menu/laboratorio-estudios/modals/modal_referencia.php
@@ -1,17 +1,17 @@
 <!-- Modal -->
 <div class="modal fade" id="modalReferencia" tabindex="-1" aria-labelledby="" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Asignar valores de referencia</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <h1>Dani no sabe nada</h1>
+
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-cancelar" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-confirmar">Guardar</button>
             </div>
         </div>
     </div>
