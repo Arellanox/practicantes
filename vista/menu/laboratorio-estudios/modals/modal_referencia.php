387c63444b515c99e7e93edfcf65cb71041ed9aa
JuanDaniel300 <118577337+JuanDaniel300@users.noreply.github.com>
2023-08-11 13:04:57 -0600

    MODAL valores de referencia update

--- vista/menu/laboratorio-estudios/modals/modal_referencia.php
+++ vista/menu/laboratorio-estudios/modals/modal_referencia.php
@@ -7,10 +7,25 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <hr>
+                <h5>Información Valores de referencia</h5>
+                <p class="none-p">Escriba el valor de referencia para el reporte...</p>
+                <div class="row mt-3">
+                    <div class="col-12 col-lg-6">
+                        <p>Dirigido a:</p>
+                        <select class="form-select input-form" name="select-genero-referencia" id="select-genero-referencia">
+                            <option value="">HOMBRE</option>
+                            <option value="">MUJER</option>
+                            <option value="">AMBOS</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-lg-6">
 
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-cancelar" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-pantone-7541" data-bs-dismiss="modal">Cerrar</button>
                 <button type="button" class="btn btn-confirmar">Guardar</button>
             </div>
         </div>
