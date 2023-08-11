b1ca0fc58716cd13f3ef0dd8a95996f1ff66d2cc
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-08-11 13:08:59 -0600

    select referencia

--- vista/menu/laboratorio-estudios/modals/controlador.js
+++ vista/menu/laboratorio-estudios/modals/controlador.js
@@ -4,6 +4,7 @@ $.post("modals/a_modals.php", function (html) {
   // Modal para agregar estudio
   $.getScript("modals/js/estu_agregar_estudio.js");
   $.getScript("modals/js/gp_rellenar_estudio.js");
+  $.getScript("modals/js/modal_referencia.js");
 
 
   getAreaUnValor('metodos', 'metodo', 'laboratorio_metodos_api', 'ID_METODO', '#MODAL_METODOS_VISTA')
