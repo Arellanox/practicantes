1a5babfc323666dc758659248c41294b3bc0e2c3
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-08-14 11:47:49 -0600

    confirmar-btn

--- vista/menu/laboratorio-estudios/modals/js/modal_referencia.js
+++ vista/menu/laboratorio-estudios/modals/js/modal_referencia.js
@@ -1,5 +1,6 @@
-//Varibales globales
-var html
+select2('#select-operador-referencia', 'modalReferencia')
+rellenarSelect("#select-operador-referencia", "valores_referencia_api", 2, "ID_OPERADORES_LOGICOS", "DESCRIPCION");
+
 
 // Detecta si lo que esta escribiendo es negativo si es asi lo manda a la verga
 $(document).ready(function () {
@@ -10,9 +11,6 @@ $(document).ready(function () {
     });
 });
 
-select2('#select-operador-referencia','modalReferencia')
-rellenarSelect("#select-operador-referencia", "valores_referencia_api", 2, "ID_OPERADORES_LOGICOS", "DESCRIPCION");  
-
 //Desactiva los imput de maximo y minimo de edad
 $('#SinEdad').on('click', function (e) {
     var minimaReferencia = $('#edad-minima-referencia');
@@ -22,15 +20,12 @@ $('#SinEdad').on('click', function (e) {
         minimaReferencia.addClass('disable-element');
         maximaReferencia.addClass('disable-element');
 
-       limpiarInputs('SinEdad', true)
+        limpiarInputs('SinEdad', true)
     } else {
         minimaReferencia.removeClass('disable-element');
         maximaReferencia.removeClass('disable-element');
     }
 })
-
-// $('#cambioReferencia').on('click', function () {
-
 
 $(document).on('change, keyup, click', '#cambioReferencia', function () {
 
@@ -40,76 +35,48 @@ $(document).on('change, keyup, click', '#cambioReferencia', function () {
         $('#cambio-rango-referencia').fadeOut(1);
 
 
-       limpiarInputs('cambioReferencia', true)
+        limpiarInputs('cambioReferencia', true)
     } else {
         $('#resultado-select-rango').fadeOut(1);
         $('#cambio-rango-referencia').fadeIn(1);
 
-       limpiarInputs('cambioReferencia', false)
-
-
+        limpiarInputs('cambioReferencia', false)
     }
 })
 
-$('#btn-agregar-vista-previa, #SinEdad ,#cambioReferencia').on('click', function () {
-
-    selectReferencia = $('#select-genero-referencia').val()
-    edadMinima = $('#edad-minima-referencia').val()
-    edadMaxima = $('#edad-maxima-referencia').val()
-
-    // if ($(this).is(':checked')) {
-    //     console.log(1)
-    // } else {
-    //     console.log(2)
-    // }
-    let btn = $(this)
-    console.log(btn.attr('id'))
-
-    switch (btn.attr('id')) {
-
-    case 'SinEdad':
-
-            // alert(1)
-        break;
-    case 'cambioReferencia':
-
-            // alert(2)
-        break;
-    }
-
-
-})
-
-$(document).on('click','#btn-guardar-referencia', function(e){
+$(document).on('click', '#btn-guardar-referencia', function (e) {
     e.preventDefault();
-
-    dataJson = {
-        api: 1,
-        servicio_id : array_selected['ID_SERVICIO'] 
-    }
-
-    ajaxAwaitFormData(dataJson, 'valores_referencia_api', 'formGuardarReferencia', { callbackAfter: true }, false, function (data) {
-        alertToast(text, 'success', 4000)
-    })
+    alertMensajeConfirm({
+        title: '¿Esta seguro de guardar los valores de referencia?',
+        text: 'No podra modificarlo',
+        icon: 'info',
+        showCancelButton: true,
+    }, function () {
+        ajaxAwaitFormData({
+            api: 1,
+            servicio_id: array_selected['ID_SERVICIO']
+        }, 'valores_referencia_api', 'formGuardarReferencia', { callbackAfter: true }, false, function (data) {
+            alertToast(text, 'success', 4000)
+        })
+    }, 1)
 
 })
 
-
-function limpiarInputs(elementID, isChecked){
-    switch(elementID){
-    case 'SinEdad':
-        if (isChecked){
-            $('#edad-maxima-referencia').val('')
-            $('#edad-minima-referencia').val('')
-        } 
-        break;
-    case 'cambioReferencia' :
-        if (isChecked){
-            $('#valor_minimo').val('')
-            $('#valor_maximo').val('')
-        } else {
-           $('#valor_referencia').val('')
-        }
-        break;
+function limpiarInputs(elementID, isChecked) {
+    switch (elementID) {
+        case 'SinEdad':
+            if (isChecked) {
+                $('#edad-maxima-referencia').val('')
+                $('#edad-minima-referencia').val('')
+            }
+            break;
+        case 'cambioReferencia':
+            if (isChecked) {
+                $('#valor_minimo').val('')
+                $('#valor_maximo').val('')
+            } else {
+                $('#valor_referencia').val('')
+            }
+            break;
     }
 }
