8409a704eee2f14225ff881194fbf586f3e0b6f6
AngelPerezJ <108588847+AngelPerezJ@users.noreply.github.com>
2023-08-14 09:12:47 -0600

    modal-referencia

--- vista/menu/laboratorio-estudios/modals/js/modal_referencia.js
+++ vista/menu/laboratorio-estudios/modals/js/modal_referencia.js
@@ -1,3 +1,6 @@
+//Varibales globales
+var html
+
 // Detecta si lo que esta escribiendo es negativo si es asi lo manda a la verga
 $(document).ready(function () {
     $(document).on('keydown', '#edad-minima-referencia, #edad-maxima-referencia', function (event) {
@@ -20,3 +23,53 @@ $('#SinEdad').on('click', function (e) {
         maximaReferencia.removeClass('disable-element');
     }
 })
+
+// $('#cambioReferencia').on('click', function () {
+
+
+
+// })
+$(document).on('click',)
+
+$(document).on('change, keyup, click', '#cambioReferencia', function () {
+
+
+    if ($(this).is(':checked')) {
+        $('#resultado-select-rango').fadeIn(1);
+        $('#cambio-rango-referencia').fadeOut(1);
+    } else {
+        $('#resultado-select-rango').fadeOut(1);
+        $('#cambio-rango-referencia').fadeIn(1);
+
+    }
+})
+
+$('#btn-agregar-vista-previa, #SinEdad ,#cambioReferencia').on('click', function () {
+
+    selectReferencia = $('#select-genero-referencia').val()
+    edadMinima = $('#edad-minima-referencia').val()
+    edadMaxima = $('#edad-maxima-referencia').val()
+
+    // if ($(this).is(':checked')) {
+    //     console.log(1)
+    // } else {
+    //     console.log(2)
+    // }
+    let btn = $(this)
+    console.log(btn.attr('id'))
+
+    switch (btn.attr('id')) {
+
+        case 'SinEdad':
+
+            alert(1)
+            break;
+        case 'cambioReferencia':
+
+            alert(2)
+            break;
+    }
+
+
+})
+
--- vista/menu/laboratorio-estudios/modals/modal_referencia.php
+++ vista/menu/laboratorio-estudios/modals/modal_referencia.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="modalReferencia" tabindex="-1" aria-labelledby="" aria-hidden="true">
-    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Asignar valores de referencia</h1>
@@ -10,72 +10,102 @@
                 <hr>
                 <h5>Información Valores de referencia</h5>
                 <p class="none-p">Escriba el valor de referencia para el reporte...</p>
+
                 <div class="row my-3">
-                    <div class="col-12 col-lg-6">
-                        <p>Dirigido a:</p>
-                        <select class="form-select input-form" name="select-genero-referencia" id="select-genero-referencia">
-                            <option selected>Elije una de las opciones disponibles</option>
-                            <option value="">HOMBRE</option>
-                            <option value="">MUJER</option>
-                            <option value="">AMBOS</option>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-4">
+                                <p>Dirigido a:</p>
+                                <select class="form-control input-form" name="select-genero-referencia"
+                                    id="select-genero-referencia">
+                                    <option selected>Elija una opcion</option>
+                                    <option value="HOMBRE">HOMBRE</option>
+                                    <option value="MUJER">MUJER</option>
+                                    <option value="AMBOS">AMBOS</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-5">
+                                <p>Edad:</p>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-form" id="edad-minima-referencia"
+                                        min="0" placeholder="Edad Minima">
+                                    <span class="input-span">-</span>
+                                    <input type="number" class="form-control input-form" id="edad-maxima-referencia"
+                                        min="0" placeholder="Edad Maxima">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <p>&nbsp;</p> <!-- Espacio en blanco para alinear con los inputs de edad -->
+                                <div class="input-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="SinEdad">
+                                        <label class="form-check-label" for="SinEdad">
+                                            Ignorar Edad
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="cambioReferencia">
+                    <label class="form-check-label" for="cambioReferencia">
+                        Cambiar a referencia
+                    </label>
+                </div>
+
+                <div class="row" id="cambio-rango-referencia">
+                    <div class="col-3">
+                        <p>Presentacion:</p>
+                        <input type="text" class="form-control input-form" name="" id="">
+                    </div>
+
+                    <div class="col-3">
+                        <p>Valor Mínimo:</p>
+                        <input type="text" class="form-control input-form" name="" id="">
+                    </div>
+
+                    <div class="col-3">
+                        <p>Valor Maximo:</p>
+                        <input type="text" class="form-control input-form" name="" id="">
+                    </div>
+
+                </div>
+
+                <div class="row" style="display: none;" id="resultado-select-rango">
+                    <div class="col-3">
+                        <p>Presentacion:</p>
+                        <input type="text" class="form-control input-form" name="" id="">
+                    </div>
+
+                    <div class="col-3">
+                        <p>Resultado es:</p>
+                        <select class="form-control input-form" name="select-genero-referencia">
+                            <option value="">&#62; Mayor que</option>
+                            <option value="">&#60; Menor que</option>
+                            <option value="">&#61; Igual</option>
                         </select>
                     </div>
 
-                    <div class="col-lg-6">
-                        <p>Edad: </p>
-                        <div class="input-group  mb-3">
-                            <input type="number" class="form-control input-form" id="edad-minima-referencia" min="0" placeholder="Edad Minima">
-                            <span class="input-span">-</span>
-                            <input type="number" class="form-control input-form" id="edad-maxima-referencia" min="0" placeholder="Edad Maxima">
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="SinEdad">
-                            <label class="form-check-label" for="SinEdad">
-                                Ignorar Edad
-                            </label>
-                        </div>
-
-                        <!-- <input type="number" id="number" name="number" placeholder='Edad Minima' class="form-input input-form" min='0'> -->
+                    <div class="col-3">
+                        <p>Valor de referencia:</p>
+                        <input type="text" class="form-control input-form" name="">
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-6">
-                        <div class="row">
-                            <div class="col-8">
-                                <p>Valor Mínimo:</p>
-                                <input type="text" class="form-control input-form" name="" id="">
-                            </div>
-                            <div class="col-4">
-                                <p>Resultado es:</p>
-                                <select class="form-select input-form" name="select-genero-referencia" id="select-genero-referencia">
-                                    <!-- <option value="">&#62; Mayor que</option> -->
-                                    <option value="">&#60; Menor que</option>
-                                    <option value="">&#61; Igual</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="row">
-                            <div class="col-8">
-                                <p>Valor Maximo:</p>
-                                <input type="text" class="form-control input-form" name="" id="">
-                            </div>
-                            <div class="col-4">
-                                <p>Resultado es:</p>
-                                <select class="form-select input-form" name="select-genero-referencia" id="select-genero-referencia">
-                                    <option value="">&#62; Mayor que</option>
-                                    <option value="">&#60; Menor que</option>
-                                    <option value="">&#61; Igual</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-            <div class="modal-footer">
+
+            <div class="border rounded-top p-1 mx-3 my-1" id="vista-previa-presentacion">
+                <p>Presentación</p>
+                <h1>Hola</h1>
+            </div>
+            <div class=" modal-footer">
                 <button type="button" class="btn btn-pantone-7541" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-confirmar" id="btn-agregar-vista-previa"><i
+                        class="bi bi-plus-circle"></i> Agregar</button>
                 <button type="button" class="btn btn-confirmar">Guardar</button>
             </div>
         </div>
@@ -84,16 +114,16 @@
 
 
 <script>
-    entrada = 20
+// entrada = 20
 
-    minimo = 60 // <=
-    maximo = 20 //<
+// minimo = 60 // <=
+// maximo = 20 //<
 
-    if (entrada >= minimo && entrada <= maximo) {
-        console.log("Esta entre el rango")
-    } else if (entrada < minimo) {
-        console.log("Es menor que el minimo")
-    } else if (entrada > maximo) {
-        console.log("Es mayor que el maximo")
-    }
+// if (entrada >= minimo && entrada <= maximo) {
+//     console.log("Esta entre el rango")
+// } else if (entrada < minimo) {
+//     console.log("Es menor que el minimo")
+// } else if (entrada > maximo) {
+//     console.log("Es mayor que el maximo")
+// }
 </script>
